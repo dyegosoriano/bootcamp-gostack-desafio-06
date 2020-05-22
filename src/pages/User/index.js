7162d501a2523a42c64eef61f576120dfdd9878c
Dyego Soriano <dyegodf2@gmail.com>
2020-05-22 16:07:11 -0300

    Listando favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useRoute } from '@react-navigation/native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Title,
+  Info,
+  Author,
+} from './styles';
 
 export default function User() {
+  const route = useRoute();
+  const { user } = route.params;
+
+  const [stars, setStars] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await api.get(`/users/${user.login}/starred`);
+      setStars(response.data);
+    } catch (error) {
+      console.tron.log(error);
+    }
+  }, []);
+
+  console.tron.log(user);
+  console.tron.log(stars);
+
   return (
-    <View>
-      <Text>User</Text>
-    </View>
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>Biografia {user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={(star) => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
   );
 }
